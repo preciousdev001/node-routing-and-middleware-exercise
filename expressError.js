71bb85523fee53fda0d34b20422115cb4893f898
preciousdev001 <preciouscheng.dev@gmail.com>
2026-09-14 01:59:55 -0700

    expressError file finished

---
+++ expressError.js
@@ -0,0 +1,12 @@
+// ext normal JS error to easily add status when instance created
+
+class ExpressError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    console.error(this.stack);
+  }
+}
+
+module.exports = ExpressError;
