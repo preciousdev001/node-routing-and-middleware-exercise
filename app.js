e741fb3b4b951e5209be32c9499f73f5d724c15f
preciousdev001 <preciouscheng.dev@gmail.com>
2026-09-16 02:26:08 -0700

    updated app.js to incluse the error handlers

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./expressError");
+
+app.use(express.json());
+
+// 404 not found err here
+app.use(function (req, res, next) {
+  return new ExpressError("Content Not Found", 404);
+});
+
+// gen err handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
 
 module.exports = app;
