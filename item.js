19b772dba3652ea4de09a94486efbe4b568a2e9c
preciousdev001 <preciouscheng.dev@gmail.com>
2026-09-16 14:04:05 -0700

    updated the item.js which is like the controller

--- item.js
+++ item.js
@@ -1,4 +1,5 @@
 const items = require("./fakeDb");
+const ExpressError = require("./expressError");
 
 class Item {
   constructor(name, price) {
@@ -17,5 +18,35 @@ class Item {
   //  update found item w/matching name to data
   static update(name, data) {
     let foundItem = items.find(name);
+    if (foundItem === undefined) {
+      throw { message: "Not Found", status: 404 };
+    }
+    foundItem.name = data.name;
+    foundItem.price = data.price;
+
+    return foundItem;
+  }
+
+  //   find and return item with match name
+
+  static find(name) {
+    const foundItem = items.find((v) => v.name === name);
+
+    if (foundItem === undefined) {
+      throw { message: "Not Found", status: 404 };
+    }
+    return foundItem;
+  }
+
+  //   remove item with match id
+  static remove(name) {
+    let foundIdx = items.findIndex((v) => v.name === name);
+
+    if (foundIdx === -1) {
+      throw { message: "Not Found", status: 404 };
+    }
+    items.splice(foundIdx, 1);
   }
 }
+
+module.exports = Item;
